db266d8d9e5825b8b7e5be663e5ac2694c093b69
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-02 14:33:38 +0100

    Create Treatments, Invoice _items And Diagnosis Tables

--- schema-based-on-diagram.sql
+++ schema-based-on-diagram.sql
@@ -24,3 +24,41 @@ CREATE TABLE invoices (
   FOREIGN KEY (medical_history_id)
   REFERENCES medical_histories(id)
 );
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type VARCHAR(50),
+  name VARCHAR(50)
+);
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT fk_invoices
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id),
+
+  CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id)
+);
+
+-- Join Table To handle M:M relationships between medical_histories and treatments tables.
+
+CREATE TABLE diagnosis (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  medical_id INT,
+  treatment_id INT,
+
+  CONSTRAINT fk_medical_histories
+  FOREIGN KEY (medical_id)
+  REFERENCES medical_histories(id),
+
+  CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id)
+);
